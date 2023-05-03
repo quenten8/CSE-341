ca69c01bc1a46e83d01856a0af1fc3abc7b4e974
quenten8 <quenten.cox00@outlook.com>
2023-05-03 15:32:19 -0700

    Week3

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,4 +5,10 @@ routes.get('/contacts', controller.getContacts);
 
 routes.get('/contacts/:id', controller.getContact);
 
+routes.post('/contacts/add', controller.addContact);
+
+routes.put('/contacts/change/:id', controller.changeContact);
+
+routes.delete('/contacts/delete/:id', controller.deleteContact);
+
 module.exports = routes;
