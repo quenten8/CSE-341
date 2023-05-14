d3bd1ace491f443a158e54b75926d45f869270e2
quenten8 <quenten.cox00@outlook.com>
2023-05-13 20:21:33 -0700

    Week4

--- server.js
+++ server.js
@@ -1,31 +1,42 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-const router = require('./routes/index.js')
+const router = require('./routes/index.js');
 
-//swagger
+// Swagger
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
+
+// Choose port
+const port = process.env.PORT || 8080;
+
+// Middleware
+app.use(bodyParser.json());
+
+// CORS handling
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+// Swagger documentation
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-//choose port
-const port = process.env.PORT || 8080
+// Routes
+app.use('/', router);
 
-//check routes
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', router);
-
-mongodb.initDb((err, mongodb) => {
-    if (err) {
-        console.log(err);
-    } else {
-        app.listen(port);
-        console.log(`Connected to DB and listening on ${port}`);
-    }
+// Initialize MongoDB connection
+mongodb.initDb((err, db) => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+  } else {
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Connected to MongoDB and listening on port ${port}`);
+    });
+  }
 });
